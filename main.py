d9e6137f70b51950ffe96592a97bd3f1ac4d9436
DanielNGit <danielnicolasmuner@gmail.com>
2020-02-12 15:04:18 -0500

    l12 We used the function input() and as well we validate entered information using the keyword IF ELSE

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ clients='Daniel,Oscar,'
 
 def new_client (client):
     global clients
-    clients+=client
-    _add_comma()
+    if client not in clients:
+       clients+=client
+       _add_comma()
+    else:
+        print('The client is already in the list')
 
 def _add_comma():
     global clients
@@ -13,9 +16,29 @@ def clients_list():
     global clients
     print(clients)
 
+def _print_welcome():
+    print('Welcome to our New Brand Interface')
+    print('*'*50)
+    print('What would you like to do today')
+    print('[C]reate a Client')
+    print('[D]elete a Client')
+
 if __name__ == "__main__":
-  new_client ('Homero')
-  clients_list()
+    _print_welcome()
+    command= input()
+    if command =='C':
+        client_name= input('What is the client Name?')
+        new_client(client_name)
+        clients_list()
+    elif command =='D':
+        pass
+    else: 
+        print('Ivalid command')
+
+
+  
+
+
 
 
 
